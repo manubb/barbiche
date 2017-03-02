bf6d8abdc41eec7ff429c84d1acebf45dd971ba6
Manuel Baclet <manuel@edagames.com>
2017-03-03 00:46:57 +0100

    Remove jquery dependency

--- scripts/makeDocs.js
+++ scripts/makeDocs.js
@@ -12,16 +12,14 @@ var indexContent = fs.readFileSync("../docs_src/index.md", "utf-8");
 var indexHeader = fs.readFileSync("../docs_src/index_header.md", "utf-8");
 var ReadMeHeader = fs.readFileSync("../docs_src/README_header.md", "utf-8");
 var indexTemplate = fs.readFileSync("../docs_src/index.html", "utf-8");
-var jquery = fs.readFileSync("../node_modules/jquery/dist/jquery.js", "utf-8");
 var polyfill = fs.readFileSync("../polyfills/dom4.js", "utf-8");
 var barbiche = fs.readFileSync("../barbiche.min.js", "utf-8");
 var examples = require("../docs_src/examples.json");
 
 jsdom.env({
 	html: documentationTemplate,
-	src: [jquery, polyfill, barbiche],
+	src: [polyfill, barbiche],
 	done: function (err, window) {
-		var $ = window.$;
 		var document = window.document;
 		var Barbiche = window.Barbiche;
 		var target = document.querySelector('#target');
@@ -66,48 +64,54 @@ jsdom.env({
 });
 jsdom.env({
 	html: indexTemplate,
-	src: [jquery],
+	src: [polyfill],
 	done: function (err, window) {
-		var $ = window.$;
-		$('#target').append(converter.makeHtml(indexHeader + indexContent));
-		$('#target').find('ul, ol').addClass('ui list');
-		$('#target').find('h2').each(function() {
-			var header = window.document.createElement('h4');
-			header.classList.add('ui', 'horizontal', 'header', 'divider');
-			this.before(header);
+		var document = window.document;
+		var target = document.querySelector('#target');
+		target.innerHTML = converter.makeHtml(indexContent);
+		Array.prototype.slice.call(target.querySelectorAll('ul, ol')).forEach(function(list) {
+			list.classList.add('ui', 'list');
 		});
 
-		$('#target').find('pre code').each(function() {
-			var $this = $(this);
-			var classes = $(this).attr('class');
-			var update = '<div class="ui code raised segment hide"><textarea class="' + classes + '">' +
-				$(this).text().replace(/\n$/, "") + '</textarea></div>';
-			$this.parent().replaceWith(update);
+		Array.prototype.slice.call(target.querySelectorAll('h2')).forEach(function(h) {
+			var header = document.createElement('h4');
+			header.classList.add('ui', 'horizontal', 'header', 'divider');
+			h.before(header);
 		});
-		$('table').addClass('ui celled table');
+
+		Array.prototype.slice.call(target.querySelectorAll('pre code')).forEach(function(code) {
+			var classes = code.getAttribute('class');
+			var div = document.createElement('div');
+			div.classList.add('ui', 'code', 'raised', 'segment', 'hide');
+			div.innerHTML = '<textarea class="' + classes + '">' +
+				code.textContent.replace(/\n$/, "") + '</textarea>';
+			code.parentNode.replaceWith(div);
+		});
+		Array.prototype.slice.call(target.querySelectorAll('table')).forEach(function(table) {
+			table.classList.add('ui', 'celled', 'table');
+		});
 		fs.writeFileSync("../docs/index.html", serializeDocument(window.document));
   }
 });
 
 jsdom.env({
 	html: demoTemplate,
-	src: [jquery],
 	done: function (err, window) {
-		var $ = window.$;
+		var document = window.document;
 		var obj = {};
-		var buttons = $('#buttons')
+		var buttons = document.querySelector('#buttons');
 		examples.forEach(function(example) {
-			var button = window.document.createElement('button');
+			var button = document.createElement('button');
 			button.setAttribute("name", example.name);
 			button.setAttribute("class", "ui button");
-			var text = window.document.createTextNode(example.name);
+			var text = document.createTextNode(example.name);
 			button.appendChild(text);
-			buttons.append(button);
+			buttons.appendChild(button);
 			obj[example.name] = {
 				template: example.template,
 				js: example.js
 			};
-			$('#target').text("var examples = " + JSON.stringify(obj));
+			document.querySelector('#target').textContent = "var examples = " + JSON.stringify(obj);
 		});
 		fs.writeFileSync("../docs/demo.html", serializeDocument(window.document));
   }
