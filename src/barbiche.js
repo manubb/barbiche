90d454e3e843904eb5949f77e9dda3814ec28778
Manuel Baclet <manuel@edagames.com>
2017-03-19 23:24:28 +0100

    Simplify

--- src/barbiche.js
+++ src/barbiche.js
@@ -371,13 +371,8 @@ function Barbiche(opt) {
 			if (node.id) store[node.id] = this;
 			this.node = destructive ? node : node.cloneNode(true);
 			this.ready = false;
-		} else {
-			if (node !== undefined) {
-				this.node = createTemplate();
-			}
-			this.ready = true;
+			this.closures = {};
 		}
-		this.closures = {};
 		return this;
 	}
 
@@ -430,6 +425,7 @@ function Barbiche(opt) {
 		var t = new Template();
 		t.node = this.node.cloneNode(true);
 		t.closures = this.closures;
+		t.ready = true;
 		return t;
 	};
 
