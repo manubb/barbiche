bc6f8902e8d3f7c02cdcd7e44153576a091c2812
Manuel Baclet <manuel@edagames.com>
2017-03-19 21:00:44 +0100

    New build

--- barbiche.js
+++ barbiche.js
@@ -3347,7 +3347,9 @@ function Barbiche(opt) {
 							var clone = Template(closure())._clone();
 							node[order](works[DOCUMENT_FRAGMENT_NODE](clone.node.content, clone));
 						}; else return function() {
-							node[order](works[DOCUMENT_FRAGMENT_NODE](node.cloneNode(true).content, template));
+							node[order](works[DOCUMENT_FRAGMENT_NODE](
+								node.cloneNode(true).content, template
+							));
 						};
 					})(bbAttrs.import);
 
@@ -3391,7 +3393,9 @@ function Barbiche(opt) {
 						}
 					});
 				}
-				while((child = node.querySelector(prefixedGlobalAttrSelector))) {works[ELEMENT_NODE](child, template);}
+				while((child = node.querySelector(prefixedGlobalAttrSelector))) {
+					works[ELEMENT_NODE](child, template);
+				}
 			}
 			if (nodeContext) context.pop();
 		};
@@ -3400,7 +3404,9 @@ function Barbiche(opt) {
 	works[DOCUMENT_FRAGMENT_NODE] = (function() {
 		var child;
 		return function(node, template) {
-			while((child = node.querySelector(prefixedGlobalAttrSelector))) {works[ELEMENT_NODE](child, template);}
+			while((child = node.querySelector(prefixedGlobalAttrSelector))) {
+				works[ELEMENT_NODE](child, template);
+			}
 			return node;
 		};
 	})();
