bdc095487560ce27e77ba711a58b7888f02bacdc
Manuel Baclet <manuel@edagames.com>
2017-04-04 20:54:54 +0200

    Remove dom4 dependency

--- scripts/makeDocs.js
+++ scripts/makeDocs.js
@@ -12,18 +12,17 @@ var indexContent = fs.readFileSync("../docs_src/index.md", "utf-8");
 var indexHeader = fs.readFileSync("../docs_src/index_header.md", "utf-8");
 var ReadMeHeader = fs.readFileSync("../docs_src/README_header.md", "utf-8");
 var indexTemplate = fs.readFileSync("../docs_src/index.html", "utf-8");
-var polyfill = fs.readFileSync("../polyfills.min.js", "utf-8");
 var barbiche = fs.readFileSync("../barbiche.min.js", "utf-8");
 var examples = require("../docs_src/examples.json");
 
 jsdom.env({
 	html: documentationTemplate,
-	src: [polyfill, barbiche],
+	src: [barbiche],
 	done: function (err, window) {
 		var document = window.document;
 		var Barbiche = window.Barbiche;
 		var target = document.querySelector('#target');
-		target.innerHTML = converter.makeHtml(documentationContent);
+		target.innerHTML =	 converter.makeHtml(documentationContent);
 		var barbiche = Barbiche();
 		Array.prototype.slice.call(target.querySelectorAll('ul, ol')).forEach(function(list) {
 			list.classList.add('ui', 'list');
@@ -36,7 +35,7 @@ jsdom.env({
 			if (h.nodeName == 'H2') {
 				var header = window.document.createElement('h4');
 				header.classList.add('ui', 'horizontal', 'header', 'divider');
-				h.before(header);
+				h.parentNode.insertBefore(header, h);
 				h3 = [];
 				obj.h2.push({
 					id: h.getAttribute('id'),
@@ -56,7 +55,7 @@ jsdom.env({
 			div.classList.add('ui', 'code', 'raised', 'segment', 'hide');
 			div.innerHTML = '<textarea class="' + classes + '">' +
 				code.textContent.replace(/\n$/, "") + '</textarea>';
-			code.parentNode.replaceWith(div);
+			code.parentNode.parentNode.replaceChild(div, code.parentNode);
 		});
 		document.querySelector('#toc-menu').appendChild(barbiche('toc-template').merge(obj));
 		fs.writeFileSync("../docs/documentation.html", serializeDocument(window.document));
@@ -64,7 +63,6 @@ jsdom.env({
 });
 jsdom.env({
 	html: indexTemplate,
-	src: [polyfill],
 	done: function (err, window) {
 		var document = window.document;
 		var target = document.querySelector('#target');
@@ -76,7 +74,7 @@ jsdom.env({
 		Array.prototype.slice.call(target.querySelectorAll('h2')).forEach(function(h) {
 			var header = document.createElement('h4');
 			header.classList.add('ui', 'horizontal', 'header', 'divider');
-			h.before(header);
+			h.parentNode.insertBefore(header, h);
 		});
 
 		Array.prototype.slice.call(target.querySelectorAll('pre code')).forEach(function(code) {
@@ -85,7 +83,7 @@ jsdom.env({
 			div.classList.add('ui', 'code', 'raised', 'segment', 'hide');
 			div.innerHTML = '<textarea class="' + classes + '">' +
 				code.textContent.replace(/\n$/, "") + '</textarea>';
-			code.parentNode.replaceWith(div);
+			code.parentNode.parentNode.replaceChild(div, code.parentNode);
 		});
 		Array.prototype.slice.call(target.querySelectorAll('table')).forEach(function(table) {
 			table.classList.add('ui', 'celled', 'table');
